35521a41dada84a97761584a0d2ed506ee28b25c
Mohsin Khan <mmhkhan753@gmail.com>
2024-09-13 18:37:25 -0400

    pushing test for Typescript

---
+++ TypescriptTest/README.md
@@ -0,0 +1,3 @@
+# TypescriptTest
+
+
---
+++ TypescriptTest/app.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!");
+// Prevent the console from closing immediately
+console.log("Press any key to exit...");
+process.stdin.resume(); // Keeps the process running until the user presses a key
