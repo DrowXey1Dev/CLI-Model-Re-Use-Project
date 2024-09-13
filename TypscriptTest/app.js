9dd13741563b661c93892e6a0ea0841d5f178fdb
Mohsin Khan <mmhkhan753@gmail.com>
2024-09-13 18:48:21 -0400

    TypescriptTest

---
+++ TypscriptTest/app.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+// Prevent the console from closing immediately
+console.log("Press any key to exit...");
+process.stdin.resume(); // Keeps the process running until the user presses a key
+//# sourceMappingURL=app.js.map
