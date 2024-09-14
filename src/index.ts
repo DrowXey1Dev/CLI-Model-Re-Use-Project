3144370227f110b812a6f47b60bc4d7764f8b407
dnishkina <dnishkina@proton.me>
2024-09-14 22:57:39 +0000

    new script to generate single executable, simple argument processing

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,20 @@
 import fs from "fs";
 import { Command } from "commander";
 
-let message: string = 'Hello World';
-console.log(message);
+const program = new Command();
+
+program
+  .version("0.0.1")
+  .parse(process.argv);
+
+  const options = program.opts();
+
+if(process.argv[2] == "install") {
+    console.log("All dependencies were installed");
+}
+else if(process.argv[2] == "test") {
+    console.log("All tests were passed");
+}
+else {
+    fs.openSync("./" + process.argv[2] + ".NDJSON", "w");
+}
