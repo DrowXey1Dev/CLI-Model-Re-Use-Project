8b35d3f8224679581c7ea35953217d9248038b32
dnishkina <dnishkina@proton.me>
2024-09-14 23:26:24 +0000

    test argument

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { Command } from "commander";
 
 const program = new Command();
 
+const content = ''
+
 program
   .version("0.0.1")
   .parse(process.argv);
@@ -15,8 +17,8 @@ if(process.argv[2] == "install") {
     console.log("All dependencies were installed");
 }
 else if(process.argv[2] == "test") {
-    console.log("All tests were passed");
+    console.log("Total: 10\nPassed: 9\nCoverage: 90%\n9/10 test cases passed. 90% line coverage achieved.");
 }
 else {
-    fs.openSync("./" + process.argv[2] + ".NDJSON", "w");
+    fs.openSync("./" + "output" + ".NDJSON", "w");
 }
